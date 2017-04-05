f539b7949b6cd9733c92aa266e5aea61b098ca3e
Stephen Chandra <ching_yong97@yahoo.com>
2017-04-05 10:28:31 +0800

    try1.1

--- num.py
+++ num.py
@@ -1,2 +1,18 @@
-user_input = str(input("Enter a number (10-50)"))
-get_num(10-50)
+lower = 10
+upper = 50
+
+print("Please enter a number between {} - {}".format(lower,upper))
+
+def getNumber():
+    while True :
+        try:
+            number = int(input("Please enter a number: "))
+            while number < 10 or number > 50:
+                print("\n", "You must put a number between 10 - 50")
+                number = int(input("Please enter a number: "))
+            break
+        except ValueError:
+            print("\n", "Enter a valid number")
+    print("your number is", number)
+
+getNumber()
